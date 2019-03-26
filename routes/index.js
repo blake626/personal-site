c4013647e75b59f5182035410acf9e094ba6f0fe
Blake W <will2817@msu.edu>
2019-03-26 10:05:06 -0400

    First commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Blake Williams',
+  desc: 'Computer Science Student, Artist, and Skincare Enthusiast' });
+});
+
+module.exports = router;
