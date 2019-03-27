1b0d37b59a119d654a6df48a249c2e39196309e7
Blake W <will2817@msu.edu>
2019-03-27 00:13:32 -0400

    Added buttons for different subjects and content

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Blake Williams',
-  desc: 'Computer Science Student, Artist, and Skincare Enthusiast' });
+  desc: 'Computer Science Student, Artist, Skincare Enthusiast' });
 });
 
 module.exports = router;
